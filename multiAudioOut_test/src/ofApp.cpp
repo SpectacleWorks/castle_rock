c4e4e2414606e1d229ba0ccf428c145a56fb2f11
Brian Carlson <shop@spectacle.works>
2018-06-20 10:06:11 -0700

    Added test app that sends unique sounds to specific speakers via channels on the same soundcard

---
+++ multiAudioOut_test/src/ofApp.cpp
@@ -0,0 +1,69 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	//---------------------------------- gui
+
+	mainParams.setName("settings");
+
+	bassParams.setName("Bass");
+	for (int i = 0; i<8; i++) {
+		ofParameter<float> bassLevel;
+		bassParams.add(bassLevel.set("Speaker " + ofToString(i), 0.0, 0.0, 1.0));
+		bassLevels.push_back(bassLevel);
+	}
+
+	beatParams.setName("Beats");
+	for (int i = 0; i<8; i++) {
+		ofParameter<float> beatLevel;
+		beatParams.add(beatLevel.set("Speaker " + ofToString(i), 0.0, 0.0, 1.0));
+		beatLevels.push_back(beatLevel);
+	}
+
+	mainParams.add(bassParams);
+	mainParams.add(beatParams);
+	gui.setup(mainParams);
+
+	//---------------------------------- fmod
+
+	// Select driver (see log)
+	ofModMulti::ofFmodSelectDriver(0);
+
+	// Load sound
+	bassSound.loadSound("bass.wav", true);
+	bassSound.setLoop(true);
+
+	beatSound.loadSound("beat.wav", true);
+	beatSound.setLoop(true);
+
+	// Play it together
+	bassSound.play();
+	beatSound.play();
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	for (int i = 0; i<8; i++) {
+		bassSound.setVolumeAt(i, bassLevels[i]);
+	}
+
+	for (int i = 0; i<8; i++) {
+		beatSound.setVolumeAt(i, beatLevels[i]);
+	}
+
+	ofFmodSoundUpdate();
+
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofBackground(0);
+	gui.draw();
+}
---
+++ multiAudioOut_test/src/ofApp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxSoundPlayerMultiOutput.h"
+#include "ofxGui.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+
+	void setup();
+	void update();
+	void draw();
+
+	ofxSoundPlayerMultiOutput bassSound;
+	ofxSoundPlayerMultiOutput beatSound;
+
+	ofxPanel         gui;
+	ofParameterGroup mainParams;
+	ofParameterGroup bassParams;
+	ofParameterGroup beatParams;
+
+	vector<ofParameter<float>> bassLevels;
+	vector<ofParameter<float>> beatLevels;
+
+};
